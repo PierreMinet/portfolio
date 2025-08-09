26a199c2b8b1c7f9e79d8452075b52ce5d9a81f7
Pierre Minet <pierre.minet@outlook.fr>
2025-08-09 14:22:26 +0200

    Add footer's content and set footer's CSS

--- src/components/ArtworksGallery.js
+++ src/components/ArtworksGallery.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { motion } from "motion/react";
+
+const imgSize = 450;
 
 function ArtworksGallery() {
     const [images, setImages] = useState([]);
@@ -11,7 +14,19 @@ function ArtworksGallery() {
     }, []);
 
     return (
-        
+        <motion.article
+            style={{flexDirection:"column"}}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, transition: { duration: 1.5, type: "tween" } }}
+            viewport={{ once:"true", amount: .5 }}>
+            <div className="skills-wrapper" style={{gap:"2rem", marginBottom:"0.5rem"}}>
+                {images.map((img, index) => {
+                    <div className="artwork-wrapper">
+                        <img src={img.src} width={imgSize} height={imgSize} alt={img.alt} loading="lazy" />
+                    </div>
+                })}
+            </div>
+        </motion.article>
     )
 
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,25 @@
 import logo from '../images/logo.webp';
 import { BsFillPinMapFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 function Footer() {
     return (
         <footer>
-            <div>
+            <div style={{alignItems:"center"}}>
                 <img src={logo} width={300} height={150} alt="Logo" />
             </div>
-            <div>
-                <p>Bla</p>
+            <div className='footer-list' style={{paddingTop:"3rem"}}>
+                <ul>
+                    <h5>Navigation</h5>
+                    <Link className="footer-link" to="/">Home</Link>
+                    <Link className="footer-link" to="/projects">Projects</Link>
+                    <Link className="footer-link" to="/designs">Designs</Link>
+                    <Link className="footer-link" to="/contact">Contact</Link>
+                </ul>
+                <ul>
+                    <h5>Location</h5>
+                    <p>3870, Heers, Belgium <span style={{marginLeft:"0.2rem"}}><BsFillPinMapFill /></span></p>
+                </ul>
             </div>
         </footer>
     );
