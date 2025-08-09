33e73a1f325737a170573a16febac5c019d5fcd0
Pierre Minet <pierre.minet@outlook.fr>
2025-08-09 14:55:48 +0200

    Add contact page content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,30 @@
-function Contact() {
+import { motion } from "motion/react";
 
-}
+function Contact() {
+    return (
+        <>
+            <motion.article
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2, type: "tween" }}
+            className="hero-banner">
+                <h1>Contact Me</h1>
+            </motion.article>
+
+            <div className='separator'></div>
+
+            <motion.article
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 2, type: "tween" }}>
+                <div style={{textAlign:"center"}}>
+                    <p>For any <b>question</b>, <b>inquiry</b>, <b>offer</b> or <b>custom project</b>, please reach me by e-mail :</p>
+                    <p>pierrem.designs@gmail.com</p>
+                    <p>A contact form will be available <b>soon</b></p>
+                </div>
+            </motion.article>
+        </>
+    );
+};
 
 export default Contact;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -4,23 +4,23 @@ import { motion } from "motion/react";
 function Overview() {
     return (
         <>
-        <motion.article
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.5, duration: 2, type: "tween" }}
-        >
-            <div className="half-div">
-                <div style={{display:"flex"}}>
-                    <h3>"My name is </h3><span className='h3-highlight'>Pierre</span><h3>, and this is what I do"</h3>
+            <motion.article
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 2, type: "tween" }}
+            >
+                <div className="half-div">
+                    <div style={{display:"flex"}}>
+                        <h3>"My name is </h3><span className='h3-highlight'>Pierre</span><h3>, and this is what I do"</h3>
+                    </div>
+                    <p>I am a <b>Junior Front End developer</b> based in <b>Belgium</b>, I mostly work with <b>React</b>, and I also am an <b>experienced graphic designer</b> who worked in <b>the music industry</b>.</p>
+                    <p>I have learnt and I am actively practicing <b>HTML5</b>, <b>CSS3</b>, <b>JavaScript ES6</b>, <b>XML</b>, <b>React JS</b>, <b>Git / GitHub</b>, <b>Figma</b>, <b>Photoshop CC</b>, <b>Premiere Pro</b>, and <b>After Effect</b>.</p>
+                    <p>I am actively working on various <b>web projects</b>, and learning new skills <b>as much as possible</b>.</p>
+                    </div>
+                <div className="half-div">
+                    <img src={hero} width={400} height={403} alt="Me" />
                 </div>
-                <p>I am a <b>Junior Front End developer</b> based in <b>Belgium</b>, I mostly work with <b>React</b>, and I also am an <b>experienced graphic designer</b> who worked in <b>the music industry</b>.</p>
-                <p>I have learnt and I am actively practicing <b>HTML5</b>, <b>CSS3</b>, <b>JavaScript ES6</b>, <b>XML</b>, <b>React JS</b>, <b>Git / GitHub</b>, <b>Figma</b>, <b>Photoshop CC</b>, <b>Premiere Pro</b>, and <b>After Effect</b>.</p>
-                <p>I am actively working on various <b>web projects</b>, and learning new skills <b>as much as possible</b>.</p>
-                </div>
-            <div className="half-div">
-                <img src={hero} width={400} height={403} alt="Me" />
-            </div>
-        </motion.article>
+            </motion.article>
         </>
     );
 };
